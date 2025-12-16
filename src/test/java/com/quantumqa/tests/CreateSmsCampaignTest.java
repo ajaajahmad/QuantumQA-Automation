c130bd9ca5c9eddfc2cc1031b9ce83c04db79566
ajaajahmad <ajaaj75@gmail.com>
2025-12-16 13:28:05 +0530

    Add fail-fast login validation and assertion for list selection in CreateSmsCampaignTest

--- src/test/java/com/quantumqa/tests/CreateSmsCampaignTest.java
+++ src/test/java/com/quantumqa/tests/CreateSmsCampaignTest.java
@@ -1,5 +1,6 @@
 package com.quantumqa.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.quantumqa.base.BaseTest;
@@ -9,18 +10,28 @@ public class CreateSmsCampaignTest extends BaseTest {
 
 	@Test(dataProvider = "loginData", dataProviderClass = TestDataProvider.class)
 	public void verifySmsCampaignCreation(String username, String password) {
-		loginPage.userLogin(username, password);
+
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+
 		smsCampaignPage.clickOnSmsMenu();
 		smsCampaignPage.clickOnCampaignsSubMenu();
 		smsCampaignPage.acknowledgeDltPopupWindow();
 		smsCampaignPage.clickOnCreateCampaignButton();
 		smsCampaignPage.enterCampaignName("Selenium Flow");
-		smsCampaignPage.clickOnCanpaignCategory();
-		smsCampaignPage.selectCampaingCategory();
-		smsCampaignPage.selectCampaignTyep();
+		smsCampaignPage.clickOnCampaignCategory();
+		smsCampaignPage.selectCampaignCategory();
+		smsCampaignPage.selectCampaignType();
 		smsCampaignPage.clickOnImportContacts();
 		smsCampaignPage.clickOnListTab();
-		smsCampaignPage.enterListName("ajaaj");
-		smsCampaignPage.clickOnImportButton();
+		smsCampaignPage.enterListName("Automation");
+		smsCampaignPage.selectListByName("Automation");
+		smsCampaignPage.clickOnGoButton();
+
+		boolean isSelected = smsCampaignPage.isListSelected("Automation");
+		Assert.assertTrue(isSelected, "List 'Automation' is not selected.");
 	}
 }
