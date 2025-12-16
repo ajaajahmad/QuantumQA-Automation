004f8d294b63ac8445850e52d227494ac3855d3e
ajaajahmad <ajaaj75@gmail.com>
2025-12-16 13:27:17 +0530

    Update TestDataProvider to use modified ExcelUtils for data retrieval

--- src/test/java/com/quantumqa/dataprovider/TestDataProvider.java
+++ src/test/java/com/quantumqa/dataprovider/TestDataProvider.java
@@ -1,14 +1,42 @@
 package com.quantumqa.dataprovider;
 
 import org.testng.annotations.DataProvider;
-
 import com.quantumqa.utils.ExcelUtils;
+import java.util.stream.Stream;
 
 public class TestDataProvider {
 
 	@DataProvider(name = "loginData")
 	public Object[][] loginData() {
-		return ExcelUtils.getExcelData();
+		Object[][] allData = ExcelUtils.getExcelData();
+
+		Stream<Object[]> validDataStream = Stream.of(allData).filter(this::isValidLoginRow);
+
+		Object[][] validData = validDataStream.toArray(Object[][]::new);
+
+		if (validData.length == 0) {
+			throw new RuntimeException(
+					"No valid login data found in Excel. Please check the Excel file and ensure username and password are not blank.");
+		}
+
+		return validData;
 	}
 
+	private boolean isValidLoginRow(Object[] row) {
+		if (row == null || row.length < 2) {
+			return false;
+		}
+
+		Object usernameObj = row[0];
+		if (usernameObj == null || usernameObj.toString().isBlank()) {
+			return false;
+		}
+
+		Object passwordObj = row[1];
+		if (passwordObj == null || passwordObj.toString().isBlank()) {
+			return false;
+		}
+
+		return true;
+	}
 }
