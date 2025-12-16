506d6e1fe9df6549c66684b6e3779dab16c59498
ajaajahmad <ajaaj75@gmail.com>
2025-12-16 13:26:29 +0530

    Refactor SmsCampaignPage methods to use BasePage wait utilities for consistent element handling

--- src/main/java/com/quantumqa/pages/SmsCampaignPage.java
+++ src/main/java/com/quantumqa/pages/SmsCampaignPage.java
@@ -1,9 +1,8 @@
 package com.quantumqa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.support.FindBy;
 
 import com.quantumqa.base.BasePage;
@@ -11,131 +10,113 @@ import com.quantumqa.base.BasePage;
 public class SmsCampaignPage extends BasePage {
 
 	public SmsCampaignPage(WebDriver driver) {
-
 		super(driver);
-
 	}
 
 	@FindBy(xpath = "//div[contains(@class, 'menu-event') and .//span[contains(@class, 'icon-Menu_SMS')]]")
 	private WebElement smsMenu;
+
 	@FindBy(xpath = "//span[normalize-space()='Sender IDs']/ancestor::li[1]/preceding-sibling::li[1]//span[normalize-space()='Campaigns']")
 	private WebElement smsCampaignsSubMenu;
+
 	@FindBy(xpath = "//button[contains(text(),'OK')]")
 	private WebElement smsCampaignsDltPopup;
+
 	@FindBy(xpath = "//button[@class='btn btn-design btn-send ng-star-inserted']//span[contains(text(),'Create Campaign')]")
 	private WebElement smsCreateCampaign;
+
 	@FindBy(xpath = "//div[@class='col-md-3 campaign-name ng-star-inserted']//input[@type='text']")
 	private WebElement smsCampaignName;
+
 	@FindBy(xpath = "//div[@class='ui floating dropdown labeled icon button ng-star-inserted']//span[contains(text(),'Select')]")
 	private WebElement smsCampaignCategory;
-	@FindBy(xpath = "//input[@placeholder='Create New']")
-	private WebElement smsCampaignCreateCategoryInput;
-	@FindBy(xpath = "//*[@id='Layer_1']")
-	private WebElement smsCampaignCreateCategoryAddIcon;
+
 	@FindBy(xpath = "//div[@class='scrolling menu']//div[@title='AutoSMS']")
 	private WebElement smsCampaignSelectCategory;
-	@FindBy(xpath = "//input[@placeholder='Search ...']")
-	private WebElement smsCampaignCategorySearchInput;
-	@FindBy(xpath = "//div[@title='Auto SMS']") // "//div[@title='" + categoryName + "']"; for dynamic category search.
-	private WebElement smsCampaignSelectSearchedCategory;
-	@FindBy(xpath = "//input[@value='Personalised']") // for common campaing just replace Personalised with Common
+
+	@FindBy(xpath = "//input[@value='Personalised']")
 	private WebElement smsCampaignType;
+
 	@FindBy(xpath = "//button[contains(text(),'Import Contacts')]")
 	private WebElement smsCampaignImportContacts;
+
 	@FindBy(xpath = "//span[@class='mdc-tab__text-label'][contains(text(),'Lists')]")
 	private WebElement smsCampaignListTab;
+
 	@FindBy(xpath = "//input[@placeholder='Search List']")
 	private WebElement smsCampaignListSearchInput;
+
 	@FindBy(xpath = "//button[contains(text(),'Go')]")
 	private WebElement smsCampaignListGoBtn;
-	@FindBy(id = "mat-mdc-checkbox-14-input")
-	private WebElement smsCampaingSearchedListCheckBox;
+
 	@FindBy(xpath = "//button[contains(@class,'process-file') and contains(normalize-space(),'Import')]")
 	private WebElement smsCampaignContactsImportBtn;
-	@FindBy(xpath = "//button[contains(@class,'btn-design') and contains(text(),'Choose Template')]")
-	private WebElement smsCampaignChooseTemplateBtn;
-	@FindBy(xpath = "//input[contains(@class,'filter-search')]")
-	private WebElement smsCampaingSearchTemplate;
-	@FindBy(id = "mat-radio-28-input")
-	private WebElement smsCampaignChooseTemplateRadioBtn;
-	@FindBy(css = "button.import-btn.btn-design")
-	private WebElement smsCampaignTempmateSaveBtn;
-	@FindBy(css = "button.import-btn.btn-design-white")
-	private WebElement smsCampaignTemplateCancelBtn;
-	@FindBy(css = "button.btn-design.btn-send")
-	private WebElement smsCampaingSendBtn;
-	@FindBy(xpath = "//button[@type='button' and contains(normalize-space(.),'Send Now')]")
-	private WebElement smsCampaignSendNowBtn;
 
 	public void clickOnSmsMenu() {
-		waitForElementToAppear(smsMenu);
-		smsMenu.click();
+		click(smsMenu);
 	}
 
 	public void clickOnCampaignsSubMenu() {
-		waitForElementToAppear(smsCampaignsSubMenu);
-		smsCampaignsSubMenu.click();
+		click(smsCampaignsSubMenu);
 	}
 
 	public void acknowledgeDltPopupWindow() {
-		waitForElementToAppear(smsCampaignsDltPopup);
-		smsCampaignsDltPopup.click();
+		click(smsCampaignsDltPopup);
 	}
 
 	public void clickOnCreateCampaignButton() {
-		waitForElementToAppear(smsCreateCampaign);
-		smsCreateCampaign.click();
+		click(smsCreateCampaign);
 	}
 
 	public void enterCampaignName(String campaignName) {
-		waitForElementToAppear(smsCampaignName);
-		smsCampaignName.clear();
-		smsCampaignName.sendKeys(campaignName);
+		type(smsCampaignName, campaignName);
 	}
 
-	public void clickOnCanpaignCategory() {
-		waitForElementToAppear(smsCampaignCategory);
-		smsCampaignCategory.click();
+	public void clickOnCampaignCategory() {
+		click(smsCampaignCategory);
 	}
 
-	public void selectCampaingCategory() {
-		waitForElementToAppear(smsCampaignSelectCategory);
-		smsCampaignSelectCategory.click();
+	public void selectCampaignCategory() {
+		click(smsCampaignSelectCategory);
 	}
 
-	public void selectCampaignTyep() {
-		waitForElementToAppear(smsCampaignType);
-		smsCampaignType.click();
+	public void selectCampaignType() {
+		click(smsCampaignType);
 	}
 
 	public void clickOnImportContacts() {
-		waitForElementToAppear(smsCampaignImportContacts);
-		smsCampaignImportContacts.click();
+		click(smsCampaignImportContacts);
 	}
 
 	public void clickOnListTab() {
-		waitForElementToAppear(smsCampaignListTab);
-		smsCampaignListTab.click();
+		click(smsCampaignListTab);
 	}
 
 	public void enterListName(String listName) {
-		waitForElementToAppear(smsCampaignListSearchInput);
-		smsCampaignListSearchInput.sendKeys(listName);
+		type(smsCampaignListSearchInput, listName);
 	}
 
-	public void clickOnSearchButton() {
-		waitForElementToBeClickable(smsCampaignListGoBtn);
-		smsCampaignListGoBtn.click();
+	public void clickOnGoButton() {
+		click(smsCampaignListGoBtn);
 	}
 
-	public void selectListName() {
-		waitForElementToAppear(smsCampaingSearchedListCheckBox);
-		smsCampaingSearchedListCheckBox.click();
+	public void selectListByName(String listName) {
+		String checkboxXpath = String.format("//span[normalize-space()='%s']/ancestor::tr//mat-checkbox", listName);
+		WebElement checkbox = wait.until(driver -> driver.findElement(By.xpath(checkboxXpath)));
+		safeClick(checkbox);
+	}
+
+	public boolean isListSelected(String listName) {
+		String checkboxXpath = String.format("//span[normalize-space()='%s']/ancestor::tr//mat-checkbox", listName);
+		WebElement checkbox = driver.findElement(By.xpath(checkboxXpath));
+		return checkbox.getAttribute("class").contains("mat-mdc-checkbox-checked");
 	}
 
 	public void clickOnImportButton() {
-		waitForElementToBeClickable(smsCampaignContactsImportBtn);
-		smsCampaignContactsImportBtn.click();
+		click(smsCampaignContactsImportBtn);
 	}
 
+	private void safeClick(WebElement element) {
+		((org.openqa.selenium.JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+	}
 }
