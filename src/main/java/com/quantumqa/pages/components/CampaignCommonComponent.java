7716907b5885f554e24f02ffba79a76836ef8af0
ajaajahmad <ajaaj75@gmail.com>
2025-12-19 00:00:16 +0530

    refactor campaign common component class

--- src/main/java/com/quantumqa/pages/components/CampaignCommonComponent.java
+++ src/main/java/com/quantumqa/pages/components/CampaignCommonComponent.java
@@ -1,55 +1,54 @@
-//package com.quantumqa.pages.components;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//import com.quantumqa.base.BasePage;
-//
-//public class CampaignCommonComponent extends BasePage {
-//
-//	public CampaignCommonComponent(WebDriver driver, WebDriverWait wait) {
-//		super(driver);
-//	}
-//
-//	@FindBy(xpath = "//button[@class='btn btn-design btn-send ng-star-inserted']//span[contains(text(),'Create Campaign')]")
-//	private WebElement createCampaignButton;
-//
-//	@FindBy(xpath = "//div[@class='col-md-3 campaign-name ng-star-inserted']//input[@type='text']")
-//	private WebElement campaignNameInputBox;
-//
-//	@FindBy(xpath = "//div[@class='ui floating dropdown labeled icon button ng-star-inserted']//span[contains(text(),'Select')]")
-//	private WebElement campaignCategoryDropdown;
-//
-//	@FindBy(xpath = "//input[@placeholder='Create New']")
-//	private WebElement campaignCreateCategoryInput;
-//
-//	@FindBy(xpath = "//*[@id='Layer_1']")
-//	private WebElement campaignCreateCategoryAddIcon;
-//
-//	@FindBy(xpath = "//div[@class='scrolling menu']//div[@title='AutowSMS']")
-//	private WebElement campaignSelectCategory;
-//
-//	@FindBy(xpath = "//input[@placeholder='Search ...']")
-//	private WebElement campaignCategorySearchInputBox;
-//
-//	@FindBy(xpath = "//div[@title='Auto SMS']") // "//div[@title='" + categoryName + "']"; could be used for dynamic
-//												// category search.
-//	private WebElement campaignSelectSearchedCategory;
-//
-//	public void clickOnCreateCampaignButton() {
-//		click(createCampaignButton);
-//	}
-//
-//	public void enterCampaignName(String campaignName) {
-//		type(campaignNameInputBox, campaignName);
-//	}
-//
-//	public void selectCampaignCategory(String categoryText) {
-//		click(campaignCategoryDropdown);
-//		type(campaignCreateCategoryInput, categoryText);
-//		click(campaignCreateCategoryAddIcon);
-//	}
-//
-//}
+package com.quantumqa.pages.components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.quantumqa.base.BasePage;
+
+public class CampaignCommonComponent extends BasePage {
+
+	public CampaignCommonComponent(WebDriver driver, WebDriverWait wait) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "//button[@class='btn btn-design btn-send ng-star-inserted']//span[contains(text(),'Create Campaign')]")
+	private WebElement createCampaignButton;
+
+	@FindBy(xpath = "//div[@class='col-md-3 campaign-name ng-star-inserted']//input[@type='text']")
+	private WebElement campaignNameInputBox;
+
+	@FindBy(xpath = "//div[@class='ui floating dropdown labeled icon button ng-star-inserted']//span[contains(text(),'Select')]")
+	private WebElement campaignCategoryDropdown;
+
+	@FindBy(xpath = "//input[@placeholder='Create New']")
+	private WebElement campaignCreateCategoryInput;
+
+	@FindBy(xpath = "//*[@id='Layer_1']")
+	private WebElement campaignCreateCategoryAddIcon;
+
+	@FindBy(xpath = "//div[@class='scrolling menu']//div[@title='AutowSMS']")
+	private WebElement campaignSelectCategory;
+
+	@FindBy(xpath = "//input[@placeholder='Search ...']")
+	private WebElement campaignCategorySearchInputBox;
+
+	@FindBy(xpath = "//div[@title='Auto SMS']")
+	private WebElement campaignSelectSearchedCategory;
+
+	public void clickOnCreateCampaignButton() {
+		click(createCampaignButton);
+	}
+
+	public void enterCampaignName(String campaignName) {
+		type(campaignNameInputBox, campaignName);
+	}
+
+	public void selectCampaignCategory(String categoryText) {
+		click(campaignCategoryDropdown);
+		type(campaignCreateCategoryInput, categoryText);
+		click(campaignCreateCategoryAddIcon);
+	}
+
+}
