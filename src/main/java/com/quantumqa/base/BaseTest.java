6184de432749cb33733b262d33baa7736b39a9ac
ajaajahmad <ajaaj75@gmail.com>
2025-12-25 19:17:08 +0530

    added cockies deletion before login

--- src/main/java/com/quantumqa/base/BaseTest.java
+++ src/main/java/com/quantumqa/base/BaseTest.java
@@ -40,6 +40,7 @@ public class BaseTest {
 			}
 
 			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
 
