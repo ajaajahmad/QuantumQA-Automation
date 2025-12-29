2bffd18b7969e7ff3ac45bd13cf45bf5c31e6531
ajaajahmad <ajaaj75@gmail.com>
2025-12-29 17:05:56 +0530

    added json data processer

--- src/test/java/com/quantumqa/dataprovider/TestDataProvider.java
+++ src/test/java/com/quantumqa/dataprovider/TestDataProvider.java
@@ -1,22 +1,27 @@
 package com.quantumqa.dataprovider;
 
-import org.testng.annotations.DataProvider;
-import com.quantumqa.utils.ExcelUtils;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Stream;
+
+import org.testng.annotations.DataProvider;
+
+import com.quantumqa.utils.ExcelDataReader;
+import com.quantumqa.utils.JsonDataReader;
 
 public class TestDataProvider {
 
-	@DataProvider(name = "loginData")
-	public Object[][] loginData() {
-		Object[][] allData = ExcelUtils.getExcelData();
+	@DataProvider(name = "excelUserData")
+	public Object[][] excelUserData() {
+		Object[][] allData = ExcelDataReader.getExcelData();
 
 		Stream<Object[]> validDataStream = Stream.of(allData).filter(this::isValidLoginRow);
 
 		Object[][] validData = validDataStream.toArray(Object[][]::new);
 
 		if (validData.length == 0) {
-			throw new RuntimeException(
-					"No valid login data found in Excel. Please check the Excel file and ensure username and password are not blank.");
+			throw new RuntimeException("No valid login data found in Excel. Please check the Excel file.");
 		}
 
 		return validData;
@@ -26,17 +31,34 @@ public class TestDataProvider {
 		if (row == null || row.length < 2) {
 			return false;
 		}
-
 		Object usernameObj = row[0];
 		if (usernameObj == null || usernameObj.toString().isBlank()) {
 			return false;
 		}
-
 		Object passwordObj = row[1];
 		if (passwordObj == null || passwordObj.toString().isBlank()) {
 			return false;
 		}
-
 		return true;
+	}
+
+	@DataProvider(name = "jsonUserData")
+	public Object[][] getJsonUserData() throws IOException {
+
+		JsonDataReader jsonReader = new JsonDataReader();
+		String filePath = System.getProperty("user.dir") + "/src/test/resources/testdata/usercredentials.json";
+
+		List<HashMap<String, String>> dataList = jsonReader.getJsonDataToMap(filePath);
+
+		Object[][] dataArray = new Object[dataList.size()][2];
+
+		for (int i = 0; i < dataList.size(); i++) {
+			HashMap<String, String> userData = dataList.get(i);
+
+			dataArray[i][0] = userData.get("username");
+			dataArray[i][1] = userData.get("password");
+		}
+
+		return dataArray;
 	}
 }
