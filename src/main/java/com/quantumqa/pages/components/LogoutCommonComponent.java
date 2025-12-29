ad76e184ec714d107d6efc552965eff1f1d94689
ajaajahmad <ajaaj75@gmail.com>
2025-12-29 14:03:41 +0530

    added common logout conmponent

---
+++ src/main/java/com/quantumqa/pages/components/LogoutCommonComponent.java
@@ -0,0 +1,25 @@
+package com.quantumqa.pages.components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import com.quantumqa.base.BasePage;
+
+public class LogoutCommonComponent extends BasePage {
+
+	public LogoutCommonComponent(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(css = ".user-name-alias")
+	private WebElement profileIcon;
+
+	@FindBy(xpath = "//a[contains(text(),'Logout')]")
+	private WebElement logoutButton;
+
+	public void userLogout() {
+		click(profileIcon);
+		click(logoutButton);
+	}
+
+}
