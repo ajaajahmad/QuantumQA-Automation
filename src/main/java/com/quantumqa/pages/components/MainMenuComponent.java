2918a0cae9ccf76ffb82a4282144a8ec74fe1d19
ajaajahmad <ajaaj75@gmail.com>
2025-12-31 14:57:10 +0530

    added functions and for main menu and sub menu

--- src/main/java/com/quantumqa/pages/components/MainMenuComponent.java
+++ src/main/java/com/quantumqa/pages/components/MainMenuComponent.java
@@ -1,5 +1,32 @@
 package com.quantumqa.pages.components;
 
-public class MainMenuComponent {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.quantumqa.base.BasePage;
+
+public class MainMenuComponent extends BasePage {
+
+	public MainMenuComponent(WebDriver driver, WebDriverWait wait) {
+		super(driver);
+	}
+
+	@FindBy(css = "div.custom-new-menu-wrapper")
+	private WebElement sideBar;
+
+	public void clickMainMenu(String menuName) {
+		WebElement menu = sideBar
+				.findElement(By.xpath(".//span[@class='menu-heading' and normalize-space()='" + menuName + "']"));
+		menu.click();
+	}
+
+	public void clickSubMenu(String parentMenu, String childMenu) {
+		WebElement subMenu = sideBar.findElement(By.xpath(
+				".//span[normalize-space()='" + parentMenu + "']" + "/ancestor::li[contains(@class,'menu-list-items')]"
+						+ "//span[normalize-space()='" + childMenu + "']"));
+		subMenu.click();
+	}
 }
