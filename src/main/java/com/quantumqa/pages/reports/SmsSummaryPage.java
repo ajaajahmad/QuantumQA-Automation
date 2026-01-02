8818e7dfbed35d8b7fd8369d2424a839970f57d4
ajaajahmad <ajaaj75@gmail.com>
2026-01-02 22:09:14 +0530

    added generic view verification function

--- src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
+++ src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
@@ -1,6 +1,8 @@
 package com.quantumqa.pages.reports;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.quantumqa.base.BasePage;
 import com.quantumqa.pages.components.MainMenuComponent;
@@ -13,8 +15,20 @@ public class SmsSummaryPage extends BasePage {
 		super(driver);
 		this.menu = new MainMenuComponent(driver);
 	}
-	
+
+	private By header = By.xpath("//div[contains(@class,'mis_table_header')]");
+
+	private By viewOption(String view) {
+		return By.xpath(".//li[span[normalize-space()='" + view + "']]");
+	}
+
 	public void clickOnSmsSummary() {
 		menu.navigate("Reports", "SMS", "Summary");
+	}
+
+	public void clickOnViewBy(String view) {
+		WebElement headerEl = driver.findElement(header);
+		WebElement option = headerEl.findElement(viewOption(view));
+		click(option);
 	}
 }
