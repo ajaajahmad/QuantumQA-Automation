add03bba457accbc945dc434c9979aa72938be26
ajaajahmad <ajaaj75@gmail.com>
2026-01-02 20:48:51 +0530

    added test class to execute report test cases

---
+++ src/test/java/com/quantumqa/tests/ReportsTest.java
@@ -0,0 +1,22 @@
+package com.quantumqa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+
+public class ReportsTest extends BaseTest {
+
+	@Test(groups = {
+			"sms_summary" }, enabled = true, dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void verifySmsSummary(String username, String password) {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+
+		smsSummaryPage.clickOnSmsSummary();
+	}
+}
