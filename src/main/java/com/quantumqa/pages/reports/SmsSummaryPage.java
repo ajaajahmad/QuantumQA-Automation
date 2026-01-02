9a3f4d5a3ab090cea61b98f987a288a68d1c10e7
ajaajahmad <ajaaj75@gmail.com>
2026-01-02 20:43:51 +0530

    added function navigate report menu

--- src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
+++ src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
@@ -1,5 +1,20 @@
 package com.quantumqa.pages.reports;
 
-public class SmsSummaryPage {
+import org.openqa.selenium.WebDriver;
 
+import com.quantumqa.base.BasePage;
+import com.quantumqa.pages.components.MainMenuComponent;
+
+public class SmsSummaryPage extends BasePage {
+
+	private MainMenuComponent menu;
+
+	public SmsSummaryPage(WebDriver driver) {
+		super(driver);
+		this.menu = new MainMenuComponent(driver);
+	}
+	
+	public void clickOnSmsSummary() {
+		menu.navigate("Reports", "SMS", "Summary");
+	}
 }
