d6827162790d626486270cf7e63b08c6bd924c64
ajaajahmad <ajaaj75@gmail.com>
2026-01-05 21:28:44 +0530

    refactored for data driven view by option

--- src/test/java/com/quantumqa/tests/ReportsTest.java
+++ src/test/java/com/quantumqa/tests/ReportsTest.java
@@ -1,6 +1,7 @@
 package com.quantumqa.tests;
 
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.quantumqa.base.BaseTest;
@@ -8,16 +9,24 @@ import com.quantumqa.dataprovider.TestDataProvider;
 
 public class ReportsTest extends BaseTest {
 
-	@Test(groups = {
-			"sms_summary" }, enabled = true, dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
-	public void verifySmsSummary(String username, String password, String view) {
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) {
 		try {
 			loginPage.userLogin(username, password);
 		} catch (IllegalArgumentException e) {
 			Assert.fail("Login failed: " + e.getMessage());
 		}
+	}
 
-		smsSummaryPage.clickOnSmsSummary();
-		smsSummaryPage.clickOnViewBy(view);
+	@Test(groups = "sms_summary_viewby", dependsOnGroups = "user_login", dataProvider = "smsSummaryViewPanelData", dataProviderClass = TestDataProvider.class)
+	public void verifySmsSummaryViewBy(String view) {
+
+		smsSummaryPage.openSmsSummary();
+
+		boolean actionTaken = smsSummaryPage.selectViewByIfPresent(view);
+
+		if (!actionTaken) {
+			throw new SkipException("View By not present in UI: " + view);
+		}
 	}
 }
