95e123c1e4b03ebd334c2ddcf9b558a77609cbdb
ajaajahmad <ajaaj75@gmail.com>
2026-01-05 22:15:01 +0530

    added log4j2 for better logging

--- src/main/java/com/quantumqa/utils/LogUtils.java
+++ src/main/java/com/quantumqa/utils/LogUtils.java
@@ -3,9 +3,9 @@ package com.quantumqa.utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LogMaganer {
+public class LogUtils {
 
-	private static final Logger logger = LogManager.getLogger(LogMaganer.class);
+	private static final Logger logger = LogManager.getLogger(LogUtils.class);
 
 	public void info(String message) {
 		logger.info(message);
