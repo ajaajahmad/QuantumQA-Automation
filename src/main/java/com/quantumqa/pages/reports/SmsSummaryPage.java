dec4b078daa9e6dcda1aaefa6e6e6af063cae2f5
ajaajahmad <ajaaj75@gmail.com>
2026-01-08 23:44:18 +0530

    refacored the code

--- src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
+++ src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
@@ -1,72 +1,40 @@
 package com.quantumqa.pages.reports;
 
-import java.time.Duration;
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.quantumqa.base.BasePage;
 import com.quantumqa.pages.components.MainMenuComponent;
+import com.quantumqa.pages.components.ViewByComponent;
 
 public class SmsSummaryPage extends BasePage {
 
-	private MainMenuComponent menu;
+	private final MainMenuComponent menu;
+	private final ViewByComponent viewBy;
 
-	private String[] viewOptions = { "Date & Campaign", "Date", "Campaign" };
+	private final String[] viewOptions = { "Date & Campaign", "Date", "Campaign" };
 
 	public SmsSummaryPage(WebDriver driver) {
 		super(driver);
 		this.menu = new MainMenuComponent(driver);
+		this.viewBy = new ViewByComponent(driver);
 	}
-
-	@FindBy(id = "more_con")
-	private WebElement dropdown;
 
 	public void openSmsSummary() {
 		menu.navigate("Reports", "SMS", "Summary");
 		sleep();
 	}
 
-	public void selectViewOptionsAndDropdowns() throws InterruptedException {
+	public void applyAllViewByOptions() {
 
-		for (String viewOption : viewOptions) {
-			clickViewOption(viewOption);
+		for (String option : viewOptions) {
+			viewBy.selectViewByTab(option);
 		}
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
 		for (int i = 0; i < 11; i++) {
-
-			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("loader-wrapper")));
-			click(dropdown);
-
-			List<WebElement> options = wait
-					.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("ul.bottom_drop li")));
-
-			options = driver.findElements(By.cssSelector("ul.bottom_drop li"));
-
-			if (options.size() > i) {
-				WebElement option = options.get(i);
-				option.click();
-				sleep();
-			} else {
-				System.out.println("Index " + i + " out of bounds for the options list");
+			boolean selected = viewBy.selectMoreViewByOption(i);
+			if (!selected) {
 				break;
 			}
-
-			sleep();
 		}
-	}
-
-	private void clickViewOption(String option) throws InterruptedException {
-		WebElement viewOptionElement = driver
-				.findElement(By.xpath("//li[contains(@class, 'ng-star-inserted')]/span[text()='" + option + "']"));
-		viewOptionElement.click();
-		sleep();
 	}
 }
