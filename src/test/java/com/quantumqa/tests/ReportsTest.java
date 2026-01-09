11ab3541030ca6ddea0cdfa90ffee943a5b5fcca
ajaajahmad <ajaaj75@gmail.com>
2026-01-09 21:22:26 +0530

    updated code to capture screenshot after every test step

--- src/test/java/com/quantumqa/tests/ReportsTest.java
+++ src/test/java/com/quantumqa/tests/ReportsTest.java
@@ -3,12 +3,21 @@ package com.quantumqa.tests;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.quantumqa.base.BaseTest;
 import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
 
 public class ReportsTest extends BaseTest {
+
+	private String screenshotTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		screenshotTitle = DateTimeManager.appendLocalDateTime("screenshot");
+	}
 
 	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
 	public void userLogin(String username, String password) throws IOException {
@@ -20,10 +29,10 @@ public class ReportsTest extends BaseTest {
 	}
 
 	@Test(groups = "sms_summary_viewby", dependsOnGroups = "user_login")
-	public void verifySmsSummaryViewBy() throws InterruptedException {
+	public void verifySmsSummaryViewBy() throws InterruptedException, IOException {
 
 		smsSummaryPage.openSmsSummary();
-		smsSummaryPage.applyAllViewByOptions();
+		smsSummaryPage.applyAllViewByOptions(screenshotTitle);
 
 	}
 }
