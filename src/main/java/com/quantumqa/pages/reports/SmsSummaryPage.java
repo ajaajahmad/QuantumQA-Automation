2997edfb2a37bec87e9ddc2e1583d095513ee25f
ajaajahmad <ajaaj75@gmail.com>
2026-01-10 23:05:28 +0530

    added html table utils

--- src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
+++ src/main/java/com/quantumqa/pages/reports/SmsSummaryPage.java
@@ -2,11 +2,14 @@ package com.quantumqa.pages.reports;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.quantumqa.base.BasePage;
 import com.quantumqa.pages.components.MainMenuComponent;
 import com.quantumqa.pages.components.ViewByComponent;
+import com.quantumqa.utils.HtmlReportUtil;
 import com.quantumqa.utils.ScreenshotUtils;
 
 public class SmsSummaryPage extends BasePage {
@@ -16,6 +19,7 @@ public class SmsSummaryPage extends BasePage {
 	private final ViewByComponent viewBy;
 
 	private final String[] viewOptions = { "Date & Campaign", "Date", "Campaign" };
+	private final By summaryTable = By.className("data-table");
 
 	public SmsSummaryPage(WebDriver driver) {
 		super(driver);
@@ -31,16 +35,30 @@ public class SmsSummaryPage extends BasePage {
 
 	public void applyAllViewByOptions(String screenshotTitle) throws IOException {
 
+		// Standard View By tabs
 		for (String option : viewOptions) {
 			viewBy.selectViewByTab(option);
+
+			WebElement table = driver.findElement(summaryTable);
+
+			HtmlReportUtil.createHtmlTable(table, screenshotTitle + " - " + option, "reports/sms-summary",
+					option.replaceAll("\\W+", "_"));
+
+			screenshot.getScreenshot(driver, screenshotTitle + "_" + option);
 		}
 
+		// More dropdown options
 		for (int i = 0; i < 11; i++) {
 			boolean selected = viewBy.selectMoreViewByOption(i);
-			screenshot.getScreenshot(driver, screenshotTitle + "_option_" + i);
-			if (!selected) {
+			if (!selected)
 				break;
-			}
+
+			WebElement table = driver.findElement(summaryTable);
+
+			HtmlReportUtil.createHtmlTable(table, screenshotTitle + " - More Option " + i, "reports/sms-summary",
+					"More_Option_" + i);
+
+			screenshot.getScreenshot(driver, screenshotTitle + "_option_" + i);
 		}
 	}
 }
