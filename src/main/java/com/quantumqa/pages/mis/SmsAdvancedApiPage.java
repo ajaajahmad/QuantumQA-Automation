9170a9b29ed38be5ab5b56e77fde09d41af3cd7f
ajaajahmad <ajaaj75@gmail.com>
2026-01-16 00:03:52 +0530

    created new test class

---
+++ src/main/java/com/quantumqa/pages/mis/SmsAdvancedApiPage.java
@@ -0,0 +1,77 @@
+package com.quantumqa.pages.mis;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.quantumqa.base.BasePage;
+import com.quantumqa.pages.components.DatePickerComponent;
+import com.quantumqa.pages.components.MainMenuComponent;
+import com.quantumqa.pages.components.ViewByComponent;
+import com.quantumqa.utils.HtmlReportUtil;
+import com.quantumqa.utils.ScreenshotUtils;
+
+public class SmsAdvancedApiPage extends BasePage {
+
+	private final ScreenshotUtils screenshot;
+	private final MainMenuComponent menu;
+	private final ViewByComponent viewBy;
+	private final DatePickerComponent date;
+
+	private final String[] viewOptions = { "Date", "Sender ID", "Operator" };
+	private final By summaryTable = By.className("data-table");
+
+	public SmsAdvancedApiPage(WebDriver driver) {
+		super(driver);
+		this.screenshot = new ScreenshotUtils();
+		this.menu = new MainMenuComponent(driver);
+		this.viewBy = new ViewByComponent(driver);
+		this.date = new DatePickerComponent(driver);
+	}
+
+	@FindBy(xpath = "//span[contains(text(),'Via API')]")
+	private WebElement viaApiElement;
+
+	@FindBy(xpath = "//button[contains(text(),'Search')]")
+	private WebElement searchButton;
+
+	public void openSmsSummary(String startValue, String endValue) {
+		menu.navigate("Reports", "SMS", "Advanced");
+		sleep();
+		click(viaApiElement);
+		sleep();
+		date.chooseDateOnAdvancedPage(startValue, endValue);
+		searchButton.click();
+		sleep();
+	}
+
+	public void applyAllViewByOptions(String fileTitle) throws IOException {
+
+		for (String option : viewOptions) {
+			viewBy.selectViewByTab(option);
+
+			WebElement table = driver.findElement(summaryTable);
+
+			HtmlReportUtil.createHtmlTable(table, fileTitle + " - " + option, "reports/sms-advanced-api",
+					option.replaceAll("\\W+", "_"));
+
+			screenshot.getScreenshot(driver, fileTitle + "_" + option);
+		}
+
+		for (int i = 0; i < 11; i++) {
+			boolean selected = viewBy.selectMoreViewByOption(i);
+			if (!selected)
+				break;
+
+			WebElement table = driver.findElement(summaryTable);
+
+			HtmlReportUtil.createHtmlTable(table, fileTitle + " - More Option " + i, "reports/sms-advanced-api",
+					"More_Option_" + i);
+
+			screenshot.getScreenshot(driver, fileTitle + "_option_" + i);
+		}
+	}
+}
