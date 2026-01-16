204b67761c28fe56ec04cefb6529715f87e0cc8b
ajaajahmad <ajaaj75@gmail.com>
2026-01-17 00:42:15 +0530

    added new test class

---
+++ src/test/java/com/quantumqa/tests/StandaloneReporTest.java
@@ -0,0 +1,54 @@
+package com.quantumqa.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
+
+public class StandaloneReporTest extends BaseTest {
+
+	private String fileTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		fileTitle = DateTimeManager.appendLocalDateTime("sms_summary_panel");
+	}
+
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) throws IOException {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+	}
+
+	@Test(groups = "sms_summary", enabled = true, dependsOnGroups = "user_login")
+	public void verifySmsSummaryPanel() throws InterruptedException, IOException {
+
+		smsSummaryPanelPage.openSmsSummary("01/01/2026 - 01/15/2026");
+		smsSummaryPanelPage.applyAllViewByOptions(fileTitle);
+
+	}
+
+	@Test(groups = "sms_summary", enabled = false, dependsOnGroups = "user_login")
+	public void verifySmsSummaryApi() throws InterruptedException, IOException {
+
+		smsSummaryApiPage.openSmsSummary("01/01/2026 - 01/15/2026");
+		smsSummaryApiPage.applyAllViewByOptions(fileTitle);
+
+	}
+
+	@Test(groups = "sms_summary", enabled = false, dependsOnGroups = "user_login")
+	public void verifySmsAdvanced() throws InterruptedException, IOException {
+
+		smsAdvancedPanelPage.openSmsAdvanced("2026-01-01", "2026-01-15");
+		smsAdvancedPanelPage.applyAllViewByOptions(fileTitle);
+
+	}
+}
