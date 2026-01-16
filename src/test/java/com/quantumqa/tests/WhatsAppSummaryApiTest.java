42d86e2e107fcb1da5347a6cb3d8ad4e2f97e2f2
ajaajahmad <ajaaj75@gmail.com>
2026-01-17 00:42:07 +0530

    added new test class

---
+++ src/test/java/com/quantumqa/tests/WhatsAppSummaryApiTest.java
@@ -0,0 +1,38 @@
+package com.quantumqa.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
+
+public class WhatsAppSummaryApiTest extends BaseTest {
+
+	private String fileTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		fileTitle = DateTimeManager.appendLocalDateTime("whatsapp_summary_api");
+	}
+
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) throws IOException {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+	}
+
+	@Test()
+	public void verifyWhatsAppSummaryPanel() throws InterruptedException, IOException {
+
+		whatsAppSummaryApiPage.openWhatsAppSummary("01/01/2026 - 01/15/2026");
+		whatsAppSummaryApiPage.applyAllViewByOptions(fileTitle);
+
+	}
+}
