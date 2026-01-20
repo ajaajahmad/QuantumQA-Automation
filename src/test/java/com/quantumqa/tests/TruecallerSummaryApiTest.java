16dd8a246f03b35fe68d216f09551026c5535c6c
ajaajahmad <ajaaj75@gmail.com>
2026-01-20 11:12:20 +0530

    added tc test class

---
+++ src/test/java/com/quantumqa/tests/TruecallerSummaryApiTest.java
@@ -0,0 +1,38 @@
+package com.quantumqa.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
+
+public class TruecallerSummaryApiTest extends BaseTest {
+
+	private String fileTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		fileTitle = DateTimeManager.appendLocalDateTime("trucaller_summary_api");
+	}
+
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) throws IOException {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+	}
+
+	@Test()
+	public void verifyTruecallerSummaryPanel() throws InterruptedException, IOException {
+
+		truecallerSummaryApiPage.openTruecallerSummary("01/01/2026 - 01/15/2026");
+		truecallerSummaryApiPage.applyAllViewByOptions(fileTitle);
+
+	}
+}
