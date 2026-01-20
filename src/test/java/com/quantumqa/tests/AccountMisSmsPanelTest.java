03adcaf29c2d1a0be316ba276fe537e1c368af84
ajaajahmad <ajaaj75@gmail.com>
2026-01-20 11:18:07 +0530

    added account mis sms panel test class

---
+++ src/test/java/com/quantumqa/tests/AccountMisSmsPanelTest.java
@@ -0,0 +1,38 @@
+package com.quantumqa.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
+
+public class AccountMisSmsPanelTest extends BaseTest {
+
+	private String fileTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		fileTitle = DateTimeManager.appendLocalDateTime("account_mis_sms_panel");
+	}
+
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) throws IOException {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+	}
+
+	@Test()
+	public void verifyAccountMisSmsPanel() throws InterruptedException, IOException {
+
+		userAccountMisSmsPanelPage.openUserAccountMisSmsPanel("2026-01-01", "2026-01-15");
+		userAccountMisSmsPanelPage.applyAllViewByOptions(fileTitle);
+
+	}
+}
