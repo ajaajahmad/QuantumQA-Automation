5b4097f3282e773bd2655ade37a90eebd754972c
ajaajahmad <ajaaj75@gmail.com>
2026-01-20 11:11:35 +0530

    added rcs test class

---
+++ src/test/java/com/quantumqa/tests/RcsAdvancedApiTest.java
@@ -0,0 +1,38 @@
+package com.quantumqa.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
+
+public class RcsAdvancedApiTest extends BaseTest {
+
+	private String fileTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		fileTitle = DateTimeManager.appendLocalDateTime("rcs_advanced_api");
+	}
+
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) throws IOException {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+	}
+
+	@Test()
+	public void verifyRcsAdvancedApi() throws InterruptedException, IOException {
+
+		rcsAdvancedApiPage.openRcsAdvanced("2026-01-01", "2026-01-15");
+		rcsAdvancedApiPage.applyAllViewByOptions(fileTitle);
+
+	}
+}
