978fb3617f52356ba431ae95aed03c4fb3719db6
ajaajahmad <ajaaj75@gmail.com>
2026-01-21 14:39:51 +0530

    created waba panel account mis test class

---
+++ src/test/java/com/quantumqa/tests/AccountMisWhatsAppPanelTest.java
@@ -0,0 +1,38 @@
+package com.quantumqa.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.quantumqa.base.BaseTest;
+import com.quantumqa.dataprovider.TestDataProvider;
+import com.quantumqa.utils.DateTimeManager;
+
+public class AccountMisWhatsAppPanelTest extends BaseTest {
+
+	private String fileTitle;
+
+	@BeforeTest
+	public void generateScreenshotText() {
+		fileTitle = DateTimeManager.appendLocalDateTime("account_mis_whatsapp_panel");
+	}
+
+	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
+	public void userLogin(String username, String password) throws IOException {
+		try {
+			loginPage.userLogin(username, password);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Login failed: " + e.getMessage());
+		}
+	}
+
+	@Test()
+	public void verifyAccountMisWhatsAppPanel() throws InterruptedException, IOException {
+
+		userAccountMisWhatsAppPanelPage.openUserAccountMisWhatsAppPanel("2026-01-01", "2026-01-15");
+		userAccountMisWhatsAppPanelPage.applyAllViewByOptions(fileTitle);
+
+	}
+}
