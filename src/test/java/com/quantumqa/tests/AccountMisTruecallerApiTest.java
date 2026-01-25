8d7c97bf2ec11a02fa016a84d850c3a9acffd4af
ajaajahmad <ajaaj75@gmail.com>
2026-01-25 11:52:15 +0530

    included tc in test calss

--- src/test/java/com/quantumqa/tests/AccountMisTruecallerApiTest.java
+++ src/test/java/com/quantumqa/tests/AccountMisTruecallerApiTest.java
@@ -16,7 +16,7 @@ public class AccountMisTruecallerApiTest extends BaseTest {
 
 	@BeforeTest
 	public void generateScreenshotText() {
-		fileTitle = DateTimeManager.appendLocalDateTime("account_mis_whatsapp_api");
+		fileTitle = DateTimeManager.appendLocalDateTime("account_mis_truecaller_api");
 	}
 
 	@Test(groups = "user_login", dataProvider = "excelUserData", dataProviderClass = TestDataProvider.class)
