0adfa4bae1a69742a865c7456ad6a0db687af3a3
ajaajahmad <ajaaj75@gmail.com>
2026-02-02 21:55:43 +0530

    removed PageFactory and use By locators

--- src/main/java/com/quantumqa/pages/mis/SmsAdvancedApiPage.java
+++ src/main/java/com/quantumqa/pages/mis/SmsAdvancedApiPage.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
 import com.quantumqa.base.BasePage;
 import com.quantumqa.pages.components.DatePickerComponent;
 import com.quantumqa.pages.components.MainMenuComponent;
@@ -32,11 +30,9 @@ public class SmsAdvancedApiPage extends BasePage {
 		this.date = new DatePickerComponent(driver);
 	}
 
-	@FindBy(xpath = "//span[contains(text(),'Via API')]")
-	private WebElement viaApiElement;
+	private By viaApiElement = By.xpath("//span[contains(text(),'Via API')]");
 
-	@FindBy(xpath = "//button[contains(text(),'Search')]")
-	private WebElement searchButton;
+	private By searchButton = By.xpath("//button[contains(text(),'Search')]");
 
 	public void openSmsAdvanced(String startValue, String endValue) {
 		menu.navigate("Reports", "SMS", "Advanced");
@@ -44,7 +40,7 @@ public class SmsAdvancedApiPage extends BasePage {
 		click(viaApiElement);
 		sleep();
 		date.chooseDateOnAdvancedPage(startValue, endValue);
-		searchButton.click();
+		click(searchButton);
 		sleep();
 	}
 
